1aba07b7bc99f4dfdcfdfe5f94def15e5f78440e
slode <rob.m@myplanet.com>
2018-08-14 10:13:26 -0400

    update database config setup to use default knexfile for easier cli setup

--- config/database.js
+++ config/database.js
@@ -1,46 +1,10 @@
 'use strict'
 
-// ref: https://devhints.io/knex
-const knexfile = {
-  development: {
-    client: 'pg',
-    connection: process.env.DATABASE || 'postgres://localhost:5432/discloser-server',
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: `${ __dirname }/db/migrations`
-    },
-    seeds: {
-      directory: `${ __dirname }/db/seeds`
-    }
-  },
-  staging: {
-    client: 'postgresql',
-    connection: process.env.DATABASE,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: `${ __dirname }/db/migrations`
-    },
-    seeds: {
-      directory: `${ __dirname }/db/seeds`
-    }
-  }
-}
-
+// Loading `knexfile.js` from root rather than defining it here so that knex cli
+// functions will be able to find the config in the expected default location to
+// make things a bit easier. The app itself loads this `database.js` file which
+// routes the env to the appropriate db config in the knexfile.
+const knexfile = require('../knexfile')
 const env = process.env.NODE_ENV || 'development'
 const knex = require('knex')(knexfile[env])
 
--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost:5432/discloser-server',
+    connection: process.env.DATABASE || 'postgres://localhost:5432/discloser',
     migrations: {
       tableName: 'knex_migrations',
       directory: `${ __dirname }/db/migrations`
@@ -13,14 +13,9 @@ module.exports = {
       directory: `${ __dirname }/db/seeds`
     }
   },
-  // TODO: no staging env yet; update this when ready.
   staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE,
     pool: {
       min: 2,
       max: 10
@@ -29,20 +24,19 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
-  // TODO: no production env yet; update this when ready.
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: `${ __dirname }/db/migrations`
+    },
+    seeds: {
+      directory: `${ __dirname }/db/seeds`
     }
   }
 }
